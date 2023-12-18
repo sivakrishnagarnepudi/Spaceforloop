6712d3779e928bf8b969038b9f859e66fbea2068
sivakrishnagarnepudi <gsivakrishna007@gmail.com>
2023-12-18 16:20:38 +0530

    space printing

---
+++ Space.java
@@ -0,0 +1,25 @@
+public class Space {
+    public static void main (String[]args){
+        int i, j , k=7;
+
+        for(k=7;k<12;++k){
+            for(i=0;i<k;++i)
+                System.out.print(" ");
+                for(i=0;i<8;++i)
+                    System.out.print("*");
+                    System.out.println();
+                }
+                for (i=0;i<19;++i)
+                    System.out.print("*");
+                System.out.println();
+                for (k=11;k>6;--k){
+                    for (i=0;i<k;++i)
+                        System.out.print(" ");
+                    for (i=0;i<8;++i)
+                        System.out.print("*");
+                    System.out.println();
+                }
+
+        }
+    }
+
